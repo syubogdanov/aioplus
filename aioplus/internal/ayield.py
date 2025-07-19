728ae42c2a275da18ebd891b50ead1b6e00883bc
Sergei Bogdanov <syubogdanov@outlook.com>
2025-07-19 16:52:42 +0300

    chore: add 'see also'

--- aioplus/internal/ayield.py
+++ aioplus/internal/ayield.py
@@ -2,5 +2,10 @@ import asyncio
 
 
 async def ayield() -> None:
-    """Yield control to the event loop."""
+    """Yield control to the event loop.
+
+    See Also
+    --------
+    * `asyncio.sleep(0)`.
+    """
     await asyncio.sleep(0)
