d67db92bd4995c6fd1a7a1fd785b49f50280f402
Sergei Bogdanov <syubogdanov@outlook.com>
2025-07-19 19:02:12 +0300

    feat: ayield

--- aioplus/internal/areversed.py
+++ aioplus/internal/areversed.py
@@ -2,6 +2,8 @@ from collections.abc import AsyncIterable, AsyncIterator
 from dataclasses import dataclass
 from typing import Self, TypeVar
 
+from aioplus.internal.ayield import ayield
+
 
 T = TypeVar("T")
 
@@ -48,4 +50,5 @@ class AreversedIterator(AsyncIterator[T]):
         if not self._stack:
             raise StopAsyncIteration
 
+        await ayield()
         return self._stack.pop()
