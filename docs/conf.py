ae4c9f6ff9bd8377acd617a9ffaeb948dc2281f3
Sergei Bogdanov <syubogdanov@outlook.com>
2025-07-19 21:46:23 +0300

    docs: readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = "aioplus"
+
+author = "Sergei Y. Bogdanov"
+copyright = "2025, Sergei Y. Bogdanov"
+
+html_theme = "alabaster"
+
+html_theme_options = {
+    "description": "Because asyncio.gather() is not enough!",
+    "github_user": "syubogdanov",
+    "github_repo": "aioplus",
+    "github_type": "star",
+    "github_button": True,
+    "github_banner": True,
+}
