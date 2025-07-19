4cb8361a4f3b1cfd4ee85e279c498e4f9798c66d
Sergei Bogdanov <syubogdanov@outlook.com>
2025-07-19 19:01:14 +0300

    feat: areversed

--- aioplus/__init__.py
+++ aioplus/__init__.py
@@ -1,6 +1,7 @@
 from aioplus.internal.afirst import afirst
 from aioplus.internal.alen import alen
 from aioplus.internal.arange import arange
+from aioplus.internal.areversed import areversed
 from aioplus.internal.awaitify import awaitify
 from aioplus.internal.ayield import ayield
 
@@ -14,5 +15,6 @@ __all__: list[str] = ["afirst", "alen", "arange", "awaitify", "ayield"]
 afirst.__module__ = "aioplus"
 alen.__module__ = "aioplus"
 arange.__module__ = "aioplus"
+areversed.__module__ = "aioplus"
 awaitify.__module__ = "aioplus"
 ayield.__module__ = "aioplus"
---
+++ aioplus/internal/areversed.py
@@ -0,0 +1,51 @@
+from collections.abc import AsyncIterable, AsyncIterator
+from dataclasses import dataclass
+from typing import Self, TypeVar
+
+
+T = TypeVar("T")
+
+
+def areversed(iterable: AsyncIterable[T]) -> AsyncIterable[T]:
+    """Return a reverse iterator."""
+    return AreversedIterable(iterable)
+
+
+@dataclass
+class AreversedIterable(AsyncIterable[T]):
+    """A reversed asynchronous iterable."""
+
+    iterable: AsyncIterable[T]
+
+    def __aiter__(self) -> AsyncIterator[T]:
+        """Return an asynchronous iterator."""
+        iterator = aiter(self.iterable)
+        return AreversedIterator(iterator)
+
+
+@dataclass
+class AreversedIterator(AsyncIterator[T]):
+    """A reversed asynchronous iterator."""
+
+    iterator: AsyncIterator[T]
+
+    def __post_init__(self) -> None:
+        """Initialize the object."""
+        self._stack: list[T] = []
+        self._started_flg: bool = False
+
+    def __aiter__(self) -> Self:
+        """Return an asynchronous iterator."""
+        return self
+
+    async def __anext__(self) -> T:
+        """Return the next value."""
+        if not self._started_flg:
+            self._started_flg = True
+            async for value in self.iterator:
+                self._stack.append(value)
+
+        if not self._stack:
+            raise StopAsyncIteration
+
+        return self._stack.pop()
