381b9aabe0a4fa690b0f5a9be440c554c05e7657
Sergei Bogdanov <syubogdanov@outlook.com>
2025-07-19 16:17:13 +0300

    feat: set __module__

--- aioplus/__init__.py
+++ aioplus/__init__.py
@@ -7,3 +7,8 @@ __author__ = "Sergei Y. Bogdanov <syubogdanov@outlook.com>"
 __version__ = "0.0.0"
 
 __all__: list[str] = ["arange", "awaitify", "ayield"]
+
+
+arange.__module__ = "aioplus"
+awaitify.__module__ = "aioplus"
+ayield.__module__ = "aioplus"
