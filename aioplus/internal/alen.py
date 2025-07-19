d0637ad9e6a57db7580a4f4e92fb6c9b96fbdf8c
Sergei Bogdanov <syubogdanov@outlook.com>
2025-07-19 18:35:26 +0300

    feat: use /

--- aioplus/internal/alen.py
+++ aioplus/internal/alen.py
@@ -2,7 +2,7 @@ from collections.abc import AsyncIterable
 from typing import Any
 
 
-async def alen(iterable: AsyncIterable[Any]) -> int:
+async def alen(iterable: AsyncIterable[Any], /) -> int:
     """Return the length of an object."""
     count = 0
 
