21f01a9291cd6b04b15fbf2c9d3943dc38dac0e9
Sergei Bogdanov <syubogdanov@outlook.com>
2025-07-23 00:42:37 +0300

    beauty: style

--- tests/test_areversed.py
+++ tests/test_areversed.py
@@ -4,13 +4,13 @@ from aioplus import arange, areversed
 class TestAreversed:
     """Tests for `aioplus.areversed`."""
 
-    async def test_areversed(self) -> None:
+    async def test__areversed(self) -> None:
         """Test the areversed function."""
         nums = [num async for num in areversed(arange(5))]
 
         assert nums == [4, 3, 2, 1, 0]
 
-    async def test_areversed_empty(self) -> None:
+    async def test__areversed__empty(self) -> None:
         """Test areversed with an empty range."""
         nums = [num async for num in areversed(arange(0))]
 
