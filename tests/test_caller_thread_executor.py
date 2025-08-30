8c2c43e6e46b90199f8aa8700c47dcff8607604e
Sergei Bogdanov <syubogdanov@outlook.com>
2025-08-30 12:42:37 +0300

    chore: refactoring tests: part 6

--- tests/test_caller_thread_executor.py
+++ tests/test_caller_thread_executor.py
@@ -1,61 +1,87 @@
 import pytest
 
-from aioplus import CallerThreadExecutor, anextify
+from aioplus import CallerThreadExecutor
 
 
-class TestCallerThreadExecutor:
-    """Tests for `aioplus.CallerThreadExecutor`."""
+class TestParameters:
+    """Parameter tests."""
+
+    async def test__max_workers(self) -> None:
+        """Case: non-integer."""
+        with pytest.raises(TypeError):
+            CallerThreadExecutor(max_workers="23")
+
+    async def test__max_workers__negative(self) -> None:
+        """Case: `max_workers < 0`."""
+        with pytest.raises(ValueError, match="'max_workers' must be positive"):
+            CallerThreadExecutor(max_workers=-23)
+
+    async def test__max_workers__zero(self) -> None:
+        """Case: `max_workers == 0`."""
+        with pytest.raises(ValueError, match="'max_workers' must be positive"):
+            CallerThreadExecutor(max_workers=0)
+
+    async def test__thread_name_prefix(self) -> None:
+        """Case: non-string."""
+        with pytest.raises(TypeError):
+            CallerThreadExecutor(thread_name_prefix=23)
+
+    async def test__initializer(self) -> None:
+        """Case: non-callable."""
+        with pytest.raises(TypeError):
+            CallerThreadExecutor(initializer=23)
+
+    async def test__initargs(self) -> None:
+        """Case: non-tuple."""
+        with pytest.raises(TypeError):
+            CallerThreadExecutor(initargs=23)
+
+
+class TestClass:
+    """Class tests."""
 
     async def test__caller_thread_executor__submit(self) -> None:
         """Case: submit a callable."""
         executor = CallerThreadExecutor()
 
-        future = executor.submit(lambda x: x + 1, 1)
+        future = executor.submit(lambda x: x + 4, 23)
         result = future.result()
 
-        assert result == 2
+        assert result == 27
 
     async def test__caller_thread_executor__map(self) -> None:
         """Case: map a function over an iterable."""
         executor = CallerThreadExecutor()
 
-        results = list(executor.map(lambda x: x + 1, range(5)))
+        iterator = executor.map(lambda x: x + 23, range(4))
+        results = list(iterator)
 
-        assert results == [1, 2, 3, 4, 5]
+        assert results == [23, 24, 25, 26]
 
     async def test__caller_thread_executor__submit_after_shutdown(self) -> None:
         """Case: submit a callable after shutdown."""
         executor = CallerThreadExecutor()
-        executor.shutdown()
 
+        executor.shutdown()
         with pytest.raises(RuntimeError):
-            executor.submit(lambda x: x + 1, 1)
+            executor.submit(lambda x: x + 4, 23)
 
     async def test__caller_thread_executor__map_after_shutdown(self) -> None:
         """Case: submit a callable after shutdown."""
         executor = CallerThreadExecutor()
-        executor.shutdown()
 
+        executor.shutdown()
         with pytest.raises(RuntimeError):
-            list(executor.map(lambda x: x + 1, range(5)))
+            list(executor.map(lambda x: x + 23, range(4)))
 
     async def test__caller_thread_executor__midtime_shutdown(self) -> None:
         """Case: submit a callable during shutdown."""
         executor = CallerThreadExecutor()
 
-        iterator = executor.map(lambda x: x + 1, range(5))
+        iterator = executor.map(lambda x: x + 23, range(4))
+        next(iterator)
+
         executor.shutdown()
 
         with pytest.raises(RuntimeError):
             next(iterator)
-
-    async def test__caller_thread_executor__anextify(self) -> None:
-        """Case: default behavior."""
-        executor = CallerThreadExecutor()
-
-        iterable = [1, 2, 3, 4, 5, 6, 7, 8]
-        aiterable = anextify(iterable, executor=executor)
-
-        nums = [num async for num in aiterable]
-
-        assert nums == [1, 2, 3, 4, 5, 6, 7, 8]
